f883bf8742820383831aee9103dd475191963d75
DILSHAD AHMAD <DILSHAD AHMAD@dilshad>
2020-04-16 15:16:20 +0530

    initial commit. Created a boot string backend application and configured the application property

---
+++ onlinebookstore/src/test/java/in/bhushansirgur/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bhushansirgur.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
